90be3d9eef8da576dd4c927e5a4e0217356e55e3
rpekash <ksyla@quinnipiac.edu>
2024-02-20 00:26:08 -0500

    First 2 Steps

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,7 +1,19 @@
-import java.util.Scanner;
 
 public class VendingMachine{
     public static void main(String[] args){
 
+        String[][] snacks = {
+            {"Chips","1.50"},
+            {"Protien Bar", "2.00"},
+            {"Cookies", "1.50"},
+        };
+
+        System.out.println("Avaiable Snacks;");
+        
+            for(int i = 0; i<snacks.length;i++){
+                System.out.println(snacks[i][0]+"$"+snacks[i][1]);
+            }
+        }
+
+
     }
-}
