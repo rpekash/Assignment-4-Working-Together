bf080042b54bcebf07d3c1ceef91f46a3e74cc37
rpekash <ksyla@quinnipiac.edu>
2024-02-20 20:34:35 -0500

    First 2 Stepsselection

--- VendingMachine.java
+++ VendingMachine.java
@@ -28,14 +28,12 @@ public class VendingMachine{
                     System.out.println("Enter the number of dollar bills you would like to insert.");
                     int userInput2 = scanner.nextInt();
 
-                    if(userInput2 > 0 && userInput2 >= prices[userInput]){
+                if(userInput2 > 0 && userInput2 >= prices[userInput]){
 
-                            float change = (float) (userInput2 - prices[userInput]);
-                            System.out.println("Here's your change: "+ change);
-                    }
+                    float change = (float) (userInput2 - prices[selection]);
+                    System.out.println("Here's your change: "+ change);
 
-
-
+                    }             
                 }
                 else {
                     System.out.println("Error: That number does not correspond with a snack item, please try again.");
